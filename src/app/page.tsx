e2d3ceaad4ce612f49f11f4dd43c98c186db4f0d
Abhinavanagarajan A <meetabinav@gmail.com>
2025-09-17 20:29:38 +0530

    feat: added pixel trail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Image from "next/image";
 import { useState } from "react";
+import PixelTrail from "../components/PixelTrail";
 
 export default function Home() {
   const [hasClicked, sethasClicked] = useState(false);
@@ -23,11 +24,33 @@ export default function Home() {
   }
   return (
     <main className='min-h-[100svh] bg-black flex items-center justify-between p-24' style={{ backgroundImage: 'url(/homeBg.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <>
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          zIndex: 9999,
+          pointerEvents: 'none', 
+        }}
+      >
+        <PixelTrail
+          gridSize={300}
+          trailSize={0.01}
+          maxAge={150}
+          interpolate={2.2}
+          color="#f5f69f"
+          gooeyFilter={{ id: "custom-goo-filter", strength: 2 }}
+        />
+      </div>
       <div className="w-full aspect-[330/70] m-auto relative">
         <button className="w-full aspect-video" onClick={() => sethasClicked(true)}>
           <Image src={'technoVit.svg'} alt='TechnoVit Logo' fill className='mx-auto pt-10' />
         </button>
       </div>
+      </>
     </main>
   );
 }
