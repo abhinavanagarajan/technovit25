a4f41b846f6e86060fcf72fa7b6117295aeb4116
Katyayni-Aarya <katyayni.aarya.rajput@gmail.com>
2025-10-14 13:59:51 +0530

    MerchPage Message

---
+++ src/components/MerchPage.tsx
@@ -0,0 +1,16 @@
+export const MerchPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center py-24 bg-background">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h1 className="text-5xl sm:text-6xl font-heading text-white">
+            Coming Soon
+          </h1>
+          <div className="w-20 h-1 bg-[#00ff00] mx-auto mt-4 mb-6"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MerchPage;
